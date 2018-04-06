9881fe1170d466d066d4ff209fa9e763e1639024
joshuaLoren <smallj3@outlook.com>
2018-04-05 21:12:02 -0400

    "added a controller"

---
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+
+        public string Detail()
+        {
+            return "hello from comic books controller";
+        }
+        
+    }
+}
