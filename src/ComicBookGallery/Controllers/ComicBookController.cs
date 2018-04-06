21b30b11c1b0554ff51525d425e46aa9f651d753
Joshua <smallj3@outlook.com>
2018-04-06 17:51:12 -0400

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -16,6 +16,19 @@ namespace ComicBookGallery.Controllers
          */
         public ActionResult Detail()                             //this is an action method
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Main";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+
+            ViewBag.Artists = new string[]
+            {
+                "script: dan slott",
+                "pencils: humberto ramos",
+                "inks: victor olazaba",
+                "colors: edgar delgado",
+                "letters: chris eliopoulos"
+             };
+
             //view method of the Controller class we are inheriting from
             //the View() method is used to return a ViewResult object from the action method of the controller
             return View();
