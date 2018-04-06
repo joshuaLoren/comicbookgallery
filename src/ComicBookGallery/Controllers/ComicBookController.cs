5d21ba63f89e8756325b6e54257e249b0dddea67
Joshua <smallj3@outlook.com>
2018-04-06 17:33:52 -0400

    Updated Comic Book Detail View to display comic book information

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -17,7 +17,13 @@ namespace ComicBookGallery.Controllers
         public ActionResult Detail()                             //this is an action method
         {
             //view method of the Controller class we are inheriting from
+            //the View() method is used to return a ViewResult object from the action method of the controller
             return View();
+
+            //note: inside the View folder there will be a reference to the Controller that
+            //is used to access that view. In this case, our View Folder has a Folder Named ComicBook
+            //and a Razor file (.cshtml) name Detail (the name of our action method
+
  
         }
 
